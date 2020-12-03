5c6e1f6bedb3713b83822446f80685fe6c2a002c
kary <karywong@Karys-MacBook-Air.local>
2020-12-03 18:19:01 +0000

    finish day2 part2

--- advent2p2.py
+++ advent2p2.py
@@ -3,15 +3,19 @@ def pw_is_valid(passwordline):
     least, most = list(map(int, limit_str.split('-')))
     # print(limit_str, letter, password, least, most)
     # print(password.count(letter[0]))
-    if least <= password.count(letter[0]) <= most:
+    check_front, check_back= password[least-1], password[most-1]
+    if check_front != check_back and (check_back == letter[0] or \
+        check_front == letter[0]):
+        # print(password, letter[0], check_back, check_front)
         return True
     return False
 
 with open('adv2_input.txt') as file_handle:
     all_pw = file_handle.read().splitlines()
     # print(all_pw[:5])
-    # print(pw_is_valid(all_pw[0]))
     count = 0
     for line in all_pw:
+        # print(pw_is_valid(line))
         count += 1 if pw_is_valid(line) else 0
+        # if pw_is_valid(line):
     print(count)
